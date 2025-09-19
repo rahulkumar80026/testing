7d9d5663e00cc887856f622937ab9cca14e68649
rahul <rahulkumarsw6511@gmail.com>
2025-09-19 16:06:01 +0530

    Refactor code structure for improved readability and maintainability

--- Frontend/src/components/customerView/MealHeader.jsx
+++ Frontend/src/components/customerView/MealHeader.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../assets/Images/logo1.png"
+import logo from "../../assets/Images/ashimara.png"
 
 export default function MealHeader({ day }) {
   return (
