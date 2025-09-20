a09809e9eb0f8e2d0e165215fd2fb7770ae3c944
Rahul kumar <92114290+rahulkumar80026@users.noreply.github.com>
2025-09-20 14:46:51 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:4000", // ✅ change this to your backend URL
-  baseURL: "https://testing-qnlk.onrender.com/", // ✅ change this to your backend URL
+  baseURL: "https://testing-qnlk.onrender.com", // ✅ change this to your backend URL
 });
 
 export default API;
